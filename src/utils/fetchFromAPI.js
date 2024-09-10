542d3c80436b75c9c3251ccb92fea09d4538c4b5
dheerajq <39629207+dheerajq@users.noreply.github.com>
2024-09-10 11:41:30 +0530

    Initial commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "80a06cf49dmsha7e5f2626c3ef2ap1508ffjsn6187fd2285f3",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
